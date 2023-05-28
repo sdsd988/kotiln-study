297b6577e9d7d6542ce1a59a96680959ff87ed02
JUNGSANGYUN <sdsd98987@gmail.com>
2023-05-29 01:30:34 +0900

    [코틀린] Lec01,02 복습

--- java-to-kotlin-start/src/main/kotlin/com/lannstark/lec01/Lec01Main.kt
+++ java-to-kotlin-start/src/main/kotlin/com/lannstark/lec01/Lec01Main.kt
@@ -1,13 +1,31 @@
 package com.lannstark.lec01
 
 fun main(){
+
+    /*
+     1. 변수 선언 키워드 - var, val 차이
+     모든 변수는 불변으로 선언하고, 필요한 경우 가변으로 변경
+     */
     var number1 = 10L
-    //  초기값 지정하지 않으면, 타입 명시
     val number2 = 10L
 
-    val number3:Long? = 10L
+    /*
+    2. 코틀린에서의 primitive/ reference type
+    타입을 명시적으로 작성하지 않아도, 타입이 추론됨
+     */
 
-    val Person = Person("정상윤")
+    /*
+    3. 코틀린에서의 nullable 변수
+     */
+    //코틀린은 기본적으로 모든 변수에 null 들어가지 않도록 설계, 만약 null 들어갈 수 있다면 "타입?" 사용
+    var number3:Long? = 10L
+
+    /*
+    4. 코틀린에서 객체의 인스턴스화
+    인스턴스 만들 떄, new 사용 안함
+     */
+
+    var person = Person("정상윤")
 
 
 }
